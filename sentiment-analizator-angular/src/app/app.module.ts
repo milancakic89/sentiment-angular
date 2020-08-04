721c179b2612a412316a1dc525726faeebe5241b
Milan <milancakic89@gmail.com>
2020-08-04 09:54:29 +0200

    Added basic components

--- sentiment-analizator-angular/src/app/app.module.ts
+++ sentiment-analizator-angular/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LexiconComponent } from './lexicon/lexicon.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LexiconComponent,
+    CalculatorComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
